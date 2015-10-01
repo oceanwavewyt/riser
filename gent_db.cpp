1c49fda81c8937c48a892ffe449acb07522dfc1a
oceanwavewyt <oceanwavewyt@gmail.com>
2015-10-01 21:30:19 +0800

    db参数bug

--- gent_db.cpp
+++ gent_db.cpp
@@ -189,11 +189,11 @@ bool GentDb::Init(string &err)
             options.write_buffer_size = size*1024*1024;
         }
     }	
-    options.target_file_size = TARGET_FILE_SIZE*1024*1024;
+    options.target_file_size = TARGET_FILE_SIZE;
     if(config["leveldb_target_file_size_mb"] != "") {
         int size = atoi(config["leveldb_target_file_size_mb"].c_str());
         if(size > 2) {
-            options.target_file_size = size*1024*1024;
+            options.target_file_size = size;
         }
     }
 	if(config["leveldb_block_cache_size_mb"] != "") {
